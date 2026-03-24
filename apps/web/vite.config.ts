887680fc0d19745bbda6cccc895020241ce17b49
SmartNuvem <suporte.smartnuvem@gmail.com>
2026-03-24 20:42:36 -0300

    Update vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 5173
+    port: 5173,
+    allowedHosts: [
+      "arcovan.smartnuvem.com.br",
+      "api.arcovan.smartnuvem.com.br"
+    ]
   }
 });
